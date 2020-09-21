e6152eaa5832d6240e6186f8d95b97e03c018715
Pascal Tiberghien <pascaltib@gmail.com>
2020-09-21 10:54:30 +0200

    Finished basic02

--- basic02.js
+++ basic02.js
@@ -1,12 +1,16 @@
-var array = [1, 7, -3, 9]
+var array = [1, 7, 9, -3, 6, -20, 30, 15]
 var max = 0
 for (var i = 0; i < array.length; i++) {
-   	
- 	if(max < array[i]) {
- 	max = array[i]
+
+   	console.log("i= " +i);
+   	console.log("max= " + max +"\n");
+ 	
+ 	if(max < array[i]) { /* 7< 9 */
+ 		console.log("Max changes")
+ 		max = array[i]
  	} 
- 	max = array[i]
 }
-document.write(max)
+
+console.log(max)
 
 
